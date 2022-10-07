58e587261819d4f98cb98422a9eb1db2e3c86cf4
Davide De Rosa <keeshux@gmail.com>
2022-10-07 16:41:13 +0200

    Use 2-tab indentation

--- scripts/net.rb
+++ scripts/net.rb
@@ -14,13 +14,13 @@ countries = ["US", "FR", "DE", "ES", "IT"]
 bogus_ip_prefix = "1.2.3"
 
 cfg = {
-    ep: [
-        "UDP:1194",
-        "TCP:443",
-    ],
-    frame: 1,
-    ping: 60,
-    reneg: 3600
+  ep: [
+    "UDP:1194",
+    "TCP:443",
+  ],
+  frame: 1,
+  ping: 60,
+  reneg: 3600
 }
 
 recommended_cfg = cfg.dup
@@ -30,59 +30,59 @@ recommended_cfg["auth"] = "SHA1"
 recommended_cfg["wrap"] = tls_wrap
 
 recommended = {
-    id: "default",
-    name: "Default",
-    comment: "128-bit encryption",
-    cfg: recommended_cfg
+  id: "default",
+  name: "Default",
+  comment: "128-bit encryption",
+  cfg: recommended_cfg
 }
 presets = [recommended]
 
 defaults = {
-    :username => "myusername",
-    :pool => "us",
-    :preset => "default"
+  :username => "myusername",
+  :pool => "us",
+  :preset => "default"
 }
 
 ###
 
 pools = []
 countries.each { |k|
-    id = k.downcase
-    hostname = "#{id}.sample-vpn-provider.bogus"
+  id = k.downcase
+  hostname = "#{id}.sample-vpn-provider.bogus"
 
-    addresses = nil
-    if ARGV.length > 0 && ARGV[0] == "noresolv"
-        addresses = []
-    else
-        #addresses = Resolv.getaddresses(hostname)
-        addresses = []
-        octet = 1
-        5.times {
-            ip = "#{bogus_ip_prefix}.#{octet}"
-            addresses << ip
-            octet += 1
-        }
-    end
-    addresses.map! { |a|
-        IPAddr.new(a).to_i
+  addresses = nil
+  if ARGV.length > 0 && ARGV[0] == "noresolv"
+    addresses = []
+  else
+    #addresses = Resolv.getaddresses(hostname)
+    addresses = []
+    octet = 1
+    5.times {
+      ip = "#{bogus_ip_prefix}.#{octet}"
+      addresses << ip
+      octet += 1
     }
+  end
+  addresses.map! { |a|
+    IPAddr.new(a).to_i
+  }
 
-    pool = {
-        :id => id,
-        :name => "Sample #{k}",
-        :country => k,
-        :hostname => hostname,
-        :addrs => addresses
-    }
-    pools << pool
+  pool = {
+    :id => id,
+    :name => "Sample #{k}",
+    :country => k,
+    :hostname => hostname,
+    :addrs => addresses
+  }
+  pools << pool
 }
 
 ###
 
 infra = {
-    :pools => pools,
-    :presets => presets,
-    :defaults => defaults
+  :pools => pools,
+  :presets => presets,
+  :defaults => defaults
 }
 
 puts infra.to_json
